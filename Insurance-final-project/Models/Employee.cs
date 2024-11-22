f712443f25406f197da198fc6e0773165c2182de
harshvardhan200216@gmail.com <harshvardhan200216@gmail.com>
2024-11-22 10:21:59 +0530

    save up-to-date

--- Insurance-final-project/Models/Employee.cs
+++ Insurance-final-project/Models/Employee.cs
@@ -8,10 +8,10 @@ namespace Insurance_final_project.Models
     {
         [Key]
         public Guid EmployeeId { get; set; }
-        public string EmployeeName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public int MobileNo { get; set; }
-        public double Salary { get; set; }
         public User User { get; set; } // Nav
         [ForeignKey("User")]
         public Guid UserId { get; set; }
--- Insurance-final-project/obj/Debug/net8.0/Insurance-final-project.AssemblyInfo.cs
+++ Insurance-final-project/obj/Debug/net8.0/Insurance-final-project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Insurance-final-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55384d14524a629e5b854d32d7ee8fd192d6aee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34bce46f4d851117d5ae10317d84236f1258981d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Insurance-final-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Insurance-final-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
